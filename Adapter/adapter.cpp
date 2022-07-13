f8a17674db87760aa02d48a9d852da02165b31b2
Jack Xiong <kumaxj66@gmail.com>
2022-07-13 15:06:00 +0800

    适配器模式

--- Adapter/adapter.cpp
+++ Adapter/adapter.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "adapter.h"
+#include <iostream>
+
+void Forwards::attack() {
+    cout << "前锋" << name << "进攻！" << endl;
+}
+
+void Forwards::defense() {
+    cout << "前锋" << name << "防守！" << endl;
+}
+
+const string &ForeignCenter::getName() const {
+    return name;
+}
+
+void ForeignCenter::setName(const string &name) {
+    ForeignCenter::name = name;
+}
+
+void ForeignCenter::jingong() {
+    cout << "外国中卫" << name << "进攻！" << endl;
+}
+
+void ForeignCenter::fangshou() {
+    cout << "外国中卫" << name << "防守！" << endl;
+}
--- Adapter/adapter.h
+++ Adapter/adapter.h
@@ -5,4 +5,60 @@
 #ifndef DESIGN_PATTERN_ADAPTER_H
 #define DESIGN_PATTERN_ADAPTER_H
 
+#include <string>
+
+using namespace std;
+
+class Player {
+public:
+    Player(const string &s) : name(s) {}
+
+    virtual void attack() = 0;
+
+    virtual void defense() = 0;
+
+protected:
+    string name;
+};
+
+class Forwards : public Player {
+public:
+    Forwards(const string &s) : Player(s) {}
+
+    void attack() override;
+
+    void defense() override;
+};
+
+class ForeignCenter {
+public:
+    const string &getName() const;
+
+    void setName(const string &name);
+
+    void jingong();
+
+    void fangshou();
+
+private:
+    string name;
+};
+
+class Translator : Player {
+public:
+    Translator(const string &name) : Player(name) { fc->setName(name); }
+
+    void attack() override {
+        fc->jingong();
+    }
+
+    void defense() override {
+        fc->fangshou();
+    }
+
+private:
+    ForeignCenter *fc = new ForeignCenter;
+
+};
+
 #endif //DESIGN_PATTERN_ADAPTER_H
